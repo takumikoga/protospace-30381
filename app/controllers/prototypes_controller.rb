c66a63e0176abebd35e3fc2e05e7d4e146c51bb9
takumikoga <taktaktaktechcamp@gmail.com>
2021-07-23 23:58:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,64 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
+ def index
+    @prototype = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to prototypes_path
+    else
+      render :prototype
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    if @prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+ 
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
+
+end
